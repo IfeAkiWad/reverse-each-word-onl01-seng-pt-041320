c50992107a844247bb32c27b0f2fea9ee87f36f3
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 07:11:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ reversed_array = []
   end
   reversed_array.join(" ")
 end
+
+def reverse_each_word(string)
+array = string.split
+new_array = []
+  array.collect do |word|
+    new_array << array.reverse
+  end
+  new_array.join(" ")
+end
