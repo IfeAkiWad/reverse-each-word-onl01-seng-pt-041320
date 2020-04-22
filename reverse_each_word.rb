8bb39c587582188c6d28f231ec01aff527313c81
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 07:21:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,51 +1,34 @@
-# def reverse_each_word(string)
-#   original_array = string.split(" ")
-#   return_array = []
-#   original_array.each do|string|
-#     return_array << string.reverse
-#   end
-#   return_array.join(" ")
-# end
-#
-# def reverse_each_word(string)
-#   array = string.split(" ")
-#   test_array = []
-#   array.collect do|string|
-#     test_array << string.reverse
-#   end
-#   test_array.join(" ")
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def reverse_each_word(string)
-array = string.split(" ")
-reversed_array = []
-  array.each do |word|
-    reversed_array << array.reverse
+  array = string.split(" ")
+  return_array = []
+  array.each do|word|
+    return_array << array.reverse
   end
-  reversed_array.join(" ")
+  return_array.join(" ")
 end
 
 def reverse_each_word(string)
-diff_array = string.split(" ")
-new_array = []
-  diff_array.collect do |word|
-    new_array << diff_array.reverse
+  new_array = string.split(" ")
+  diff_array = []
+  new_array.collect do|word|
+    diff_array << new_array.reverse
   end
-  new_array.join(" ")
+  diff_array.join(" ")
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
