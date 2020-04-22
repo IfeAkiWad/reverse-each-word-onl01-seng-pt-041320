8805a96abda0cdda4d3fa0196ec4c08629a41951
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 06:19:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
 array = string.split(",")
-  # array.each do |word|
-    array.reverse << array.join(" ")
-  end
+  # # array.each do |word|
+  #   array.reverse << array.join(" ")
+  # end
 end
